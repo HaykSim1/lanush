d3bae7e0d2c4a3ef7cc79b69d248ad4fc1d9f4a4
Hayk Simonyan <hayk.simonyan@betconstruct.com>
2020-04-24 15:38:52 +0400

    Add model prop

--- models/ServiceCategory.js
+++ models/ServiceCategory.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ServiceCategorySchema = new mongoose.Schema({
   title: { type: String, required: true },
   order: { type: Number, default: 0 },
+  color: { type: String, required: false },
   date: { type: Date, default: Date.now },
 });
 

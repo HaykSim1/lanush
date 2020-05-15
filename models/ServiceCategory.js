07bea3eb65233b6423d24eb01eefa99e4d91ec0c
Hayk Simonyan <hayk.simonyan@betconstruct.com>
2020-05-15 21:04:00 +0400

    Add color

--- models/ServiceCategory.js
+++ models/ServiceCategory.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ServiceCategorySchema = new mongoose.Schema({
   title: { type: String, required: true },
   order: { type: Number, default: 0 },
+  color: { type: String, default: '#ffffff' },
   date: { type: Date, default: Date.now },
 });
 
